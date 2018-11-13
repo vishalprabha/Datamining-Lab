9328801e4373805672246c4b225931b11e302fdf
vishalprabha <vishal.prabhachandar1@gmail.com>
2018-11-13 18:19:04 +0530

    r program1 altered

--- R-Programs/1.1/1.r
+++ R-Programs/1.1/1.r
@@ -1,11 +1,11 @@
-# Get and print current working directory.
-print(getwd())
+# Get and print current working directory. (optional)
+#pwd <- (getwd())
 
-# Get and print current working directory.
-print(getwd())
+# Get and print current working directory. (optional)
+#print(pwd)
 
-# Set current working directory.
-setwd("/Users/vishalprabhachandar/Documents/Programming/DataminingLab/R-Programs/1.1")
+# Set current working directory. (optional)
+#setwd("/Users/vishalprabhachandar/Documents/Programming/DataminingLab/R-Programs/1.1")
 
 # Read the csv file.
 data <- read.csv(file = "input.csv")
@@ -13,9 +13,11 @@ data <- read.csv(file = "input.csv")
 # Display contents.
 data
 
-# Get the person detail having max salary.
-retval <- subset(data, salary == max(salary))
-print(retval)
+# Get the person detail having max salary. (optional)
+#retval <- subset(data, salary == max(salary))
+#print(retval)
+#write.csv(retval, "output.csv")
+
 
 # Write data to file.
-write.csv(retval, "output.csv")
+write.csv(data, "output.csv")
