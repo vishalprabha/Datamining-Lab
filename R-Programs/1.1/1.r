8ac7f342cdbd9711b59bf3476c5a7d1623a2d2f5
vishalprabha <vishal.prabhachandar1@gmail.com>
2018-12-29 17:03:58 +0530

    Rapid miner updated

--- R-Programs/1.1/1.r
+++ R-Programs/1.1/1.r
@@ -1,22 +1,9 @@
-# Get and print current working directory. (optional)
-#pwd <- (getwd())
-
-# Get and print current working directory. (optional)
-#print(pwd)
-
-# Set current working directory. (optional)
-#setwd("/Users/vishalprabhachandar/Documents/Programming/DataminingLab/R-Programs/1.1")
 
 # Read the csv file.
 data <- read.csv(file = "input.csv")
 
 # Display contents.
 data
-
-# Get the person detail having max salary. (optional)
-#retval <- subset(data, salary == max(salary))
-#print(retval)
-#write.csv(retval, "output.csv")
 
 
 # Write data to file.
