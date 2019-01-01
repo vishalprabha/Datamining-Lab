bab4af68f7f8800dbff9c796c3ca6f5bcf2e3328
vishalprabha <vishal.prabhachandar1@gmail.com>
2019-01-01 11:53:09 +0530

    Added library factoextra in 6th R

--- R-Programs/1.6/6.R
+++ R-Programs/1.6/6.R
@@ -2,7 +2,7 @@
 
 #load libraries
 library(cluster)
-
+library(factoextra)
 
 
 #Input dataset
@@ -15,7 +15,7 @@ data$Species<-NULL
 d<- scale(dist(data,method = "euclidian"))
 #KMeans
 kfit <- kmeans(d,3)
-#HClustering 
+#HClustering
 hfit <- hkmeans(d, 3)
 fviz_cluster(kfit, data)
 fviz_cluster(hfit, data)
